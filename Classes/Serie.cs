4bcea877d88ca6a2dd8c5a62a7f3a885c7baf751
edenpaulocruz <edenpaulo@gmail.com>
2021-06-22 11:47:28 -0300

    Criação de Métodos para a Classe Serie

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace DIO.Series
 {
     public class Serie : EntidadeBase
@@ -6,5 +8,35 @@ namespace DIO.Series
         private string Titulo { get; set;}
         private string Sinopse { get; set;}
         private int Ano { get; set;}
+
+        // Métodos
+        public Serie(int id, Genero genero, string titulo, string sinopse, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Sinopse = sinopse;
+            this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Enviroment.NewLine;
+            retorno += "Título: " + this.Titulo + Enviroment.NewLine;
+            retorno += "Sinopse: " + this.Sinopse + Enviroment.NewLine;
+            retorno += "Ano de Lançamento: " + this.Ano;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
     }
 }
